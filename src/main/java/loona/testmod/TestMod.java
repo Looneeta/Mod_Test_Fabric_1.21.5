3cb424b0bb42488ec9e13a7c296138fdb716f6b2
Looneeta <lunagorzaflores@gmail.com>
2025-07-14 18:26:09 -0300

    Un comentario de testeo

--- src/main/java/loona/testmod/TestMod.java
+++ src/main/java/loona/testmod/TestMod.java
@@ -9,6 +9,8 @@ public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// *Comenta*
+
 	@Override
 	public void onInitialize() {
 
